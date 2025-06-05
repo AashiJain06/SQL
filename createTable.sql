86e45e0357b75069771c23cad4655978c036c5ab
Aashi Jain <aashijain064@gmail.com>
2025-06-05 22:48:40 +0530

    Add files via upload

---
+++ createTable.sql
@@ -0,0 +1,9 @@
+use aashi;
+create table student
+(
+  sid INTEGER NOT NULL,
+  sname TEXT not null,
+  age INTEGER NOT NULL
+);
+INSERT INTO student VALUES (1,"Aashi Jain",19);
+INSERT INTO student VALUES (2,"Riya Jain",20);
